588135ce0a94b638764dee56384a8c23c035f185
funa4i <lyhov.tim09@gmail.com>
2021-12-26 17:12:52 +0300

    cubik

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ size = width, height = 1366, 768
 screen = pygame.display.set_mode(size)
 x_col = 10
 y_col = 20
-
+fps = 60
+pos_y = 30
 
 class PlacePlay:
     # создание поля
     def __init__(self):
-        pass
+        self.pole = []
+        self.pole = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for i in range(20)]
 
     def render(self):
         pygame.draw.rect(screen, (255, 255, 255),
                          (250, 30, 350, 700), 1)
 
+    def drop_item(self):
+        if pos_y % 35 == 0:
+            pygame.draw.rect(screen, (255, 255, 255),
+                         (285, pos_y, 35, 35))
 
+clock = pygame.time.Clock()
 running = True
 play = PlacePlay()
 while running:
@@ -24,7 +31,12 @@ while running:
             running = False
     screen.fill((0, 0, 0))
     play.render()
+    play.drop_item()
     pygame.display.flip()
+    if pos_y < 694:
+        pos_y += 100 / fps
+    clock.tick(fps)
+
 
     pygame.display.flip()
 if __name__ == '__main__':
