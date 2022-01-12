ad703f9cd4b37275c6c18d4f5604dd01e3d13d11
funa4i <lyhov.tim09@gmail.com>
2022-01-12 12:16:50 +0300

    drop_figur_bez_povorota

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ y_col = 20
 fps = 60
 pos_y_global = 0
 flag = True
-kvadrat = klass_figur.Kub()
-liniya = klass_figur.Pryamaya()
-stupen_1 = klass_figur.StupenVLevo()
-stupen_2 = klass_figur.StupenVPravo()
-g_russka = klass_figur.GPravil()
-g_ne_russka = klass_figur.GNePravil()
+kvadrat_figura = klass_figur.Kub()
+liniya_figura = klass_figur.Pryamaya()
+stupen_1_figura = klass_figur.StupenVLevo()
+stupen_2_figura = klass_figur.StupenVPravo()
+g_russka_figura = klass_figur.GPravil()
+g_ne_russka_figura = klass_figur.GNePravil()
+t_figura = klass_figur.TObraz()
 instr_rand = tetris_random.FigurePool(random.randint(1, 100000000000))
 pull_figur = []
 for i in range(5):
-    pull_figur.append(instr_rand.get_figure)
+    pull_figur.append(instr_rand.get_figure())
 
 
 class PlacePlay:
@@ -42,38 +43,62 @@ class PlacePlay:
         global flag, pos_y_global
         flag = True
         pos_y_global = 0
+        print(pull_figur)
         # -------------------------
         if num == 1:
-            self.mest_polosh = copy.deepcopy(kvadrat.cord)
+            self.mest_polosh = copy.deepcopy(kvadrat_figura.cord)
         elif num == 2:
-            liniya.return_to_start_cord()
-            self.mest_polosh = copy.copy(liniya.cord)
+            liniya_figura.return_to_start_cord()
+            self.mest_polosh = copy.copy(liniya_figura.cord)
         elif num == 3:
-            stupen_1.return_to_start_cord()
-            self.mest_polosh = copy.copy(stupen_1.cord)
+            stupen_1_figura.return_to_start_cord()
+            self.mest_polosh = copy.copy(stupen_1_figura.cord)
         elif num == 4:
-            stupen_2.return_to_start_cord()
-            self.mest_polosh = copy.copy(stupen_2.cord)
+            stupen_2_figura.return_to_start_cord()
+            self.mest_polosh = copy.copy(stupen_2_figura.cord)
         elif num == 5:
-            g_russka.return_to_start_cord()
-            self.mest_polosh = copy.copy(g_russka.cord)
+            g_russka_figura.return_to_start_cord()
+            self.mest_polosh = copy.copy(g_russka_figura.cord)
         elif num == 6:
-            g_ne_russka.return_to_start_cord()
-            self.mest_polosh = copy.copy(g_ne_russka.cord)
+            g_ne_russka_figura.return_to_start_cord()
+            self.mest_polosh = copy.copy(g_ne_russka_figura.cord)
+        elif num == 7:
+            t_figura.return_to_start_cord()
+            self.mest_polosh = copy.copy(t_figura.cord)
         for i in self.mest_polosh:
             self.pole[i[0]][i[1]] = 1
 
     def drop_item(self, x_change=0, y_change=1):
-        for i in self.mest_polosh:
-            self.pole[i[0]][i[1]] = 0
-        if self.lower_cord(self.mest_polosh) != 19:
-            for i in range(len(self.mest_polosh)):
-                self.mest_polosh[i][0] += y_change
-        for i in range(len(self.mest_polosh)):
-            vrem = (self.mest_polosh[i][-1] + x_change) % 10
-            self.mest_polosh[i][-1] = vrem
-        for i in self.mest_polosh:
-            self.pole[i[0]][i[1]] = 1
+        locl_flag = True
+        locl_flag_2 = True
+        "Очень стршная проеверка 1"
+        for qw in self.mest_polosh:  # Проверка на возможность упасть
+            vr_sp = [qw[0] + 1, qw[1]]
+            if (self.pole[qw[0] + 1][qw[1]] == 1) and not vr_sp in self.mest_polosh:
+                locl_flag = False
+        "Очень стршная проеверка 2"
+        if x_change != 0:
+            for i in range(len(self.mest_polosh)):  # Проверка на возможность движение
+                vrem = (self.mest_polosh[i][-1] + x_change) % 10
+                print(vrem)
+                chek = [self.mest_polosh[i][0], vrem]
+                print(chek)
+                if self.pole[chek[0]][chek[-1]] == 1 and not chek in self.mest_polosh:
+                    locl_flag_2 = False
+        if locl_flag:
+            for i in self.mest_polosh:
+                self.pole[i[0]][i[1]] = 0
+            if self.lower_cord(self.mest_polosh) != 19:
+                for i in range(len(self.mest_polosh)):
+                    self.mest_polosh[i][0] += y_change
+            if locl_flag_2:
+                for i in range(len(self.mest_polosh)):
+                    vrem = (self.mest_polosh[i][-1] + x_change) % 10
+                    self.mest_polosh[i][-1] = vrem
+            for i in self.mest_polosh:
+                self.pole[i[0]][i[1]] = 1
+        else:
+            play.spawn_figure(take_new_figure())
 
     def lower_cord(self, sp):
         g = set()
@@ -87,7 +112,7 @@ def take_new_figure():
     pull_figur.append(instr_rand.get_figure())
     znach = pull_figur[0]
     del pull_figur[0]
-    return int(znach)  # фикс
+    return int(znach)
 
 
 clock = pygame.time.Clock()
@@ -111,8 +136,8 @@ while running:
     screen.fill((0, 0, 0))
     play.render()
     pygame.display.flip()
-    if pos_y_global < 755:
-        pos_y_global += (100 // fps) * 0.5
+    if play.lower_cord(play.mest_polosh) != 19:
+        pos_y_global += (100 // fps) * 4
         if pos_y_global % 35 == 0:
             print(pos_y_global)
             play.drop_item(0, 1)
